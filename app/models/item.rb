17373aa3b8f4bfd43dad6bab1f7148c744489915
toshi58 <toshiki.12323@gmail.com>
2020-10-26 18:02:39 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,18 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :info, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true, message: 'Half-width number'}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range'}
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :product_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
 end
