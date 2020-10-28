a7770c240c522d50149bb9ff0610cd50e5b2f558
toshi58 <toshiki.12323@gmail.com>
2020-10-28 10:32:34 +0900

    move_to_indexメソッドにもset_itemメソッドを適用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index, only: [:edit]
-  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -51,8 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == item.user_id
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def set_item
