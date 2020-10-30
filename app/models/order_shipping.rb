266a93adc67a5ec3bdde9e3d863bfaa2def93c87
toshi58 <toshiki.12323@gmail.com>
2020-10-30 11:22:20 +0900

    バリデーションの修正

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -7,7 +7,7 @@ class OrderShipping
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Include hyphen(-)"}
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A\d+\z/, message: "Input only number"}
+    validates :phone_number, format: { with: /\A\d+\z/, message: "Input only number"}, length: { maximum: 11 }
   end
   validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
 
