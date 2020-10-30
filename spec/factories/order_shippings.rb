41162184dc02e78b8e5b8d947361a85fcb5dd10b
toshi58 <toshiki.12323@gmail.com>
2020-10-30 15:42:39 +0900

    モデルの単体テストコード作成

---
+++ spec/factories/order_shippings.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_shipping do
+    token         {"tok_abcdefghijk00000000000000000"}
+    postal_code   {"123-4567"}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    city          {"東京都"}
+    address       {"1-1"}
+    building      {"建物"}
+    phone_number  {"09012345678"}
+  end
+end
