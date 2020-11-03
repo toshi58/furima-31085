6ba3c790cf21ad79c1ac5c9abb766929e4e71167
toshi58 <toshiki.12323@gmail.com>
2020-11-03 17:15:14 +0900

    アソシエーションの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one :order
-  has_one_attached :image
+  has_many_attached :images
 
   with_options presence: true do
     validates :image
